a79a57818cf7b443d9d5d5911ea070da8ed82ac8
dsharjeel <danishsharjeel7@gmail.com>
2024-06-16 14:55:47 +0500

    chore: Update user routes to include login and logout functionality

--- src/app.js
+++ src/app.js
@@ -21,6 +21,6 @@ app.use(cookieParser());
 import userRouter from "./routes/user.routes.js";
 
 // Routes declaration
-app.use("/users", userRouter);
+app.use("/api/v1/users", userRouter);
 
 export default app;
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,21 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 
+const generateAccessAndRefreshTokens = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false });
+
+        return { accessToken, refreshToken };
+    } catch (error) {
+        throw new ApiError(500, "Error generating tokens", error);
+    }
+};
+
 const registerUser = aysncHandler(async (req, res) => {
     const { fullname, email, username, password } = req.body;
     if (
@@ -54,11 +69,94 @@ const registerUser = aysncHandler(async (req, res) => {
 
     if (!createdUser) {
         throw new ApiError(500, "Error creating user");
-    };
+    }
 
     return res
         .status(201)
         .json(new ApiResponse(201, "User created successfully", createdUser));
 });
 
-export { registerUser };
+const loginUser = aysncHandler(async (req, res) => {
+    // get data from request body
+    // validate email or username
+    // check if user exists in the database
+    // check if password is correct
+    // validate user
+    // generate access and refresh token
+    // send access token in secure cookies
+
+    const { email, username, password } = req.body;
+
+    if (email && username) {
+        throw new ApiError(400, "Please provide email or username");
+    }
+
+    if (!password) {
+        throw new ApiError(400, "Please provide password");
+    }
+
+    const user = await User.findOne({ $or: [{ email }, { username }] });
+
+    if (!user) {
+        throw new ApiError(404, "User not found");
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid credentials");
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+        user._id
+    );
+
+    const logedinUser = await User.findById(user._id).select(
+        "-password -refreshToken -__v"
+    );
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    };
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    message: "User logged in successfully",
+                    accessToken: accessToken,
+                    refreshToken: refreshToken,
+                },
+                logedinUser
+            )
+        );
+});
+
+const logoutUser = aysncHandler(async (req, res) => {
+    // clear access and refresh token from cookies
+    // set refresh token to null in the database
+    // send response
+    const options = {
+        httpOnly: true,
+        secure: true,
+    };
+
+    User.findByIdAndUpdate(
+        req.user._id,
+        { $set: { refreshToken: null } },
+        { new: true }
+    );
+
+    return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, "User logged out successfully"));
+});
+
+export { registerUser, loginUser, logoutUser };
